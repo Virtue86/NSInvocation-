a0d6342a0af3669b18b227010ed34fc44aed5cd4
Virture <Virtue@VirtuedeMacBook-Pro.local>
2019-02-26 11:40:45 +0800

    提交代码

---
+++ NSInvocation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSInvocation
+//
+//  Created by Virtue on 2019/2/26.
+//  Copyright © 2019年 none. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSInvocation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSInvocation
+//
+//  Created by Virtue on 2019/2/26.
+//  Copyright © 2019年 none. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
